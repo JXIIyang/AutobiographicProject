bfeb9d69381994bd09c2ae748d61e0080b284f35
Jiaxi Yang <jy2134@nyu.edu>
2019-03-12 00:44:10 -0400

    Before Demo Build

---
+++ Midterm/Assets/PlayerShadow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShadow : MonoBehaviour
+{
+    private float _offsetX;
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offsetX = (transform.position - Player.transform.position).x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x + _offsetX, 0.83f , transform.position.z);
+    }
+}
--- Midterm/Assets/Scripts/FirstPlayerController.cs
+++ Midterm/Assets/Scripts/FirstPlayerController.cs
@@ -9,7 +9,8 @@ using UnityEngine.SceneManagement;
 
 public class FirstPlayerController : MonoBehaviour
 {
-    private Rigidbody _rb;
+    [HideInInspector]
+    public Rigidbody Rb;
     public Vector2 inputAxis;
     public Vector2 inputMouse;
 
@@ -31,8 +32,22 @@ public class FirstPlayerController : MonoBehaviour
     public List<Collider> Lines;
     public List<TextMeshProUGUI> Texts;
 
-    public GameObject Path1;
-    public GameObject Path2;
+//    public GameObject Path1;
+//    public GameObject Path2;
+    
+    public Collider Option1_1;
+    public Collider Option1_2;
+    public Collider Option2_1;
+    public Collider Option2_2;
+    public Collider Option3_1;
+    public Collider Option3_2;
+    public Collider Option4_1;
+    public Collider Option4_2;
+    public GameObject Mask1;
+    public GameObject Mask2;
+    public GameObject Mask3;
+    public GameObject Mirror;
+    
     private bool _path1Selected;
     private bool _path2Selected;
 
@@ -44,9 +59,13 @@ public class FirstPlayerController : MonoBehaviour
     
     
     public Collider SpeedUpTrigger;
-    public Collider SpeedDownTrigger;
-    public Collider Path1Trigger;
-    public Collider Path2Trigger;
+    public Collider EndTrigger;
+//    public Collider Path1Trigger;
+//    public Collider Path2Trigger;
+    public Collider Option1Trigger;
+    public Collider Option2Trigger;
+    public Collider Option3Trigger;
+    public Collider Option4Trigger;
 
 
     public bool _speedControl;
@@ -59,16 +78,21 @@ public class FirstPlayerController : MonoBehaviour
 
     public static FirstPlayerController Singleton;
 
+    private GameObject _mirror;
+
+    private bool _endGame;
+    
+
 
     
     // Start is called before the first frame update
     void Start()
     {
         Singleton = this;
-        _rb = GetComponent<Rigidbody>();
+        Rb = GetComponent<Rigidbody>();
         Volume.profile.TryGetSettings(out _depthOfField);
         LoseText.enabled = false;
-        _speedControl = true;
+//        _speedControl = true;
         Cursor.visible = false;
         GameObject[] lines = GameObject.FindGameObjectsWithTag("Line");
         Debug.Log(lines.Length);
@@ -82,6 +106,12 @@ public class FirstPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_endGame)
+        {
+            EndGame("A Game By Eleanor Yang", 0.1f);
+            return;
+        }
+        
         if (Input.GetKeyUp(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -98,15 +128,15 @@ public class FirstPlayerController : MonoBehaviour
         
         mainCamera.transform.localEulerAngles = new Vector3(- inputMouse.y * mouseSensitivity, 0, 0);
 
-        if (Input.GetMouseButton(0))
-        {
-            Fire();
-        }
+//        if (Input.GetMouseButton(0))
+//        {
+//            Fire();
+//        }
 
 
 /*        maxSpeed += 0.0001f;*/
 
-        if (_rb.velocity.magnitude > 0f)
+        if (Rb.velocity.magnitude > 0f)
         {
             _depthOfField.focusDistance.value = Mathf.Lerp(_depthOfField.focusDistance.value, 0f, 0.001f);
         }
@@ -143,7 +173,7 @@ public class FirstPlayerController : MonoBehaviour
             }
         }
 
-        SelectPath();
+//        SelectPath();
 
     }
 
@@ -153,19 +183,19 @@ public class FirstPlayerController : MonoBehaviour
         Overlay.color = new Color(0,0,0, Mathf.Lerp(Overlay.color.a, 1, rate));
     }
 
-    private void SelectPath()
-    {
-        if (_path1Selected)
-        {
-            Path2.SetActive(false);
-            Lines.Remove(Path2.GetComponent<Collider>());
-        }
-        if(_path2Selected)
-        {
-            Path1.SetActive(false);
-            Lines.Remove(Path1.GetComponent<Collider>());
-        }
-    }
+//    private void SelectPath()
+//    {
+//        if (_path1Selected)
+//        {
+//            Path2.SetActive(false);
+//            Lines.Remove(Path2.GetComponent<Collider>());
+//        }
+//        if(_path2Selected)
+//        {
+//            Path1.SetActive(false);
+//            Lines.Remove(Path1.GetComponent<Collider>());
+//        }
+//    }
     
     
 
@@ -174,13 +204,13 @@ public class FirstPlayerController : MonoBehaviour
         if (!_control) return;
         if (_speedControl)
         {
-            _rb.MovePosition(transform.position + transform.forward * inputAxis.y * forceMultiplier * 2 + transform.right * inputAxis.x * forceMultiplier);
-            _rb.MoveRotation(_rb.rotation * Quaternion.Euler(0, inputMouse.x * mouseSensitivity * 1.3f, 0));
+            Rb.MovePosition(transform.position + transform.forward * inputAxis.y * forceMultiplier * 2 + transform.right * inputAxis.x * forceMultiplier);
+            Rb.MoveRotation(Rb.rotation * Quaternion.Euler(0, inputMouse.x * mouseSensitivity * 1.3f, 0));
         }
         else
         {
-            _rb.MoveRotation(_rb.rotation * Quaternion.Euler(0, inputMouse.x * mouseSensitivity, 0));
-            _rb.MovePosition(transform.position + transform.forward * Mathf.Lerp(_rb.velocity.x, 30f, 0.01f) + transform.right * inputAxis.x * Mathf.Lerp(_rb.velocity.y, 7f, 0.03f));
+            Rb.MoveRotation(Rb.rotation * Quaternion.Euler(0, inputMouse.x * mouseSensitivity, 0));
+            Rb.MovePosition(transform.position + transform.forward * Mathf.Lerp(Rb.velocity.x, 30f, 0.01f) + transform.right * inputAxis.x * Mathf.Lerp(Rb.velocity.y, 7f, 0.03f));
         }
         
         if (OnGround)
@@ -196,7 +226,7 @@ public class FirstPlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && (OnGround || JumpTime < 0.3f))
         {
-            _rb.velocity = new Vector3(_rb.velocity.x, JumpForce, _rb.velocity.z);
+            Rb.velocity = new Vector3(Rb.velocity.x, JumpForce, Rb.velocity.z);
             OnGround = false;
         }
 
@@ -210,7 +240,7 @@ public class FirstPlayerController : MonoBehaviour
         }
         else
         {
-            _rb.angularVelocity = Vector3.zero;
+            Rb.angularVelocity = Vector3.zero;
         }
         
     }
@@ -229,7 +259,7 @@ public class FirstPlayerController : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Finish"))
         {
-/*            _rb.isKinematic = true;*/
+/*            Rb.isKinematic = true;*/
               LoseText.text = "You Win!";
               LoseText.enabled = true;
 
@@ -241,31 +271,80 @@ public class FirstPlayerController : MonoBehaviour
             _speedControl = false;
             Debug.Log("SpeedUp");
         }        
-        if (other == SpeedDownTrigger)
+        if (other == EndTrigger)
         {
             _speedControl = true;
+            if (_mirror == null)
+            {
+                _mirror = Instantiate(Mirror, new Vector3(transform.position.x + 5, 14f, 0), Quaternion.identity);
+            }
         }
-        if (other == Path1Trigger)
+//        if (other == Path1Trigger)
+//        {
+//            _path1Selected = true;
+//            _speedControl = true;
+//            
+//        }
+//        if (other == Path2Trigger)
+//        {
+//            _path2Selected = true;
+//            _speedControl = true;
+//        }
+
+        if (other == Option1Trigger)
         {
-            _path1Selected = true;
-            _speedControl = true;
-            
-        }
-        if (other == Path2Trigger)
-        {
-            _path2Selected = true;
-            _speedControl = true;
+            Option1_1.gameObject.SetActive(false);
+            Lines.Remove(Option1_1);
+            Lines.Add(Option1_2);
+            Option1_2.gameObject.SetActive(true);
+            Option2_1.gameObject.SetActive(true);
         }
         
-    }
-
-
-    private void Fire()
-    {
-        GameObject newBullet = Instantiate(BulletPrefab, transform.position + transform.forward, Quaternion.identity);
-        newBullet.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * 20, ForceMode.Impulse);
+        if (other == Option2Trigger)
+        {
+            Option2_1.gameObject.SetActive(false);
+            Lines.Remove(Option2_1);
+            Lines.Add(Option2_2);
+            Option2_2.gameObject.SetActive(true);
+            Option3_1.gameObject.SetActive(true);
+        }
         
+        if (other == Option3Trigger)
+        {
+            Option3_1.gameObject.SetActive(false);
+            Lines.Remove(Option3_1);
+            Lines.Add(Option3_2);
+            Option3_2.gameObject.SetActive(true);
+            Option4_1.gameObject.SetActive(true);
+        }
+        
+        if (other == Option4Trigger)
+        {
+            Option4_1.gameObject.SetActive(false);
+            Lines.Remove(Option4_1);
+            Lines.Add(Option4_2);
+            Option4_2.gameObject.SetActive(true);
+            Mask1.SetActive(false);
+            Mask2.SetActive(true);
+        }
+
+        if (other.CompareTag("Mirror"))
+        {
+            _endGame = true;
+            Mask3.SetActive(false);
+            Rb.isKinematic = true;
+            Rb.velocity = Vector3.zero;
+            LoseText.enabled= true;
+        }       
     }
+
+//
+//    private void Fire()
+//    {
+//        GameObject newBullet = Instantiate(BulletPrefab, transform.position + transform.forward, Quaternion.identity);
+//        newBullet.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * 20, ForceMode.Impulse);
+//        
+//    }
 
 
     private bool CheckOffTrack(out float distance)
@@ -296,7 +375,7 @@ public class FirstPlayerController : MonoBehaviour
     {
         foreach (TextMeshProUGUI text in Texts)
         {
-            if (transform.position.x - text.transform.position.x <= 50f * Mathf.Clamp(_rb.velocity.x/5, 1, 100) && transform.position.x - text.transform.position.x >= -1f)
+            if (transform.position.x - text.transform.position.x <= 50f * Mathf.Clamp(Rb.velocity.x/5, 1, 100) && transform.position.x - text.transform.position.x >= -1f)
             {
                 text.color = new Color(1, 1, 1, Mathf.Lerp(text.color.a, 1, 0.05f));
             }
